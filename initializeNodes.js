3ece2ed1ad0ac64ec235a8c0e855080f25f11c82
27sanjeevd <62220999+27sanjeevd@users.noreply.github.com>
2021-01-29 20:58:02 -0600

    Update initializeNodes.js

--- initializeNodes.js
+++ initializeNodes.js
@@ -1,7 +1,19 @@
 const random1 = require("./randomInt");
 
-function initializeNodes(){
-
+function initializeNodes(xVals, yVals){
+	let gfg = new Array(xVals.length);
 	
+	for(let x = 0; x < gfg.length; x++){
+		
+		gfg[x] = [];
+		let paths = random1(0, xVals.length / 2 - 1);
+		
+		for(let y = 0; y < paths; y++){
+			
+			let position = 
+		)
+	}
+	
+
 
 }
