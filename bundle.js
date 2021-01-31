d97110415166c0cd199c98faaf14d0b653c116d7
27sanjeevd <62220999+27sanjeevd@users.noreply.github.com>
2021-01-30 19:06:16 -0600

    Added basic graph creation

--- bundle.js
+++ bundle.js
@@ -31,24 +31,42 @@ var Run = {
 	}
 }
 function getXY(event){
+	
+	var temp1 = false;
+	
 	x = event.clientX;
 	y = event.clientY;
 	if (Run.key && Run.key[81]) {
 		Info.fillStyle = "skyblue";
 		Info.fillRect(x-10, y-10, 10, 10);
+		temp1 = true;
 	}
 			
 	else if (Run.key && Run.key[87]) {
 		Info.fillStyle = "red";
 		Info.fillRect(x-10, y-10, 10, 10);
+		temp1 = true;
 	}
 			
 	else if (Run.key && Run.key[69]) {
 		Info.fillStyle = "green";
 		Info.fillRect(x-10, y-10, 10, 10);
+		temp1 = true;
 	}
-	xNode.push(x-10);
-	yNode.push(y-10);
-	counter++;
+	else if (Run.key && Run.key[82]) {
+		for(var i = 0; i < counter - 1; i++){
+			
+			Info.moveTo(xNode[i]+5, yNode[i]+5);
+			Info.lineTo(xNode[i+1]+5, yNode[i+1]+5);
+			Info.strokeStyle = "#FF0000";
+			Info.stroke();
+		}
+	}
+	if (temp1){
+		
+		xNode.push(x-10);
+		yNode.push(y-10);
+		counter++;
+	}
 	
 }
