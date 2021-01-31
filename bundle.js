04e5dfa482a6b248214bf1597b84909e2ecd5590
27sanjeevd <62220999+27sanjeevd@users.noreply.github.com>
2021-01-30 18:55:14 -0600

    moved js out of main file

--- bundle.js
+++ bundle.js
@@ -1,4 +1,54 @@
-function bundle1(x){
+function bundle() {
+	
+	alert("HI");
+}
 
-	return x;
+
+var ColorArray = ["red", "blue"];
+var xNode = [];
+var yNode = [];
+var counter = 0;
+var canvas = "";
+var Info = "";
+var x = 500;
+var y = 500;
+
+function SetUp(){
+	canvas = document.getElementById('canvas');
+	Info = canvas.getContext('2d');
+	Run.Change();
+			
+}
+var Run = {
+	Change : function (){
+		window.addEventListener('keydown', function (e) {
+			Run.key = (Run.key || []); 										
+			Run.key[e.keyCode] = (e.type == "keydown");
+		})
+		window.addEventListener('keyup', function (e) {
+			Run.key[e.keyCode] = (e.type == "keydown");            
+		})
+	}
+}
+function getXY(event){
+	x = event.clientX;
+	y = event.clientY;
+	if (Run.key && Run.key[81]) {
+		Info.fillStyle = "skyblue";
+		Info.fillRect(x-10, y-10, 10, 10);
+	}
+			
+	else if (Run.key && Run.key[87]) {
+		Info.fillStyle = "red";
+		Info.fillRect(x-10, y-10, 10, 10);
+	}
+			
+	else if (Run.key && Run.key[69]) {
+		Info.fillStyle = "green";
+		Info.fillRect(x-10, y-10, 10, 10);
+	}
+	xNode.push(x-10);
+	yNode.push(y-10);
+	counter++;
+	
 }
